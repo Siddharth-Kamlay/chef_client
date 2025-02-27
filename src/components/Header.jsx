5f125b0169889454f696f0170fef9df1ea4e8109
Siddharth <siddharthkamlay777@gmail.com>
2025-02-27 12:27:35 +0530

    Added logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
 import logo from '../assets/logo.jpg'
-import { NavLink } from 'react-router-dom'
+import { NavLink , useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import AuthContext from './AuthContext'
 
 const Header = () => {
-  const { isAuthenticated } = useContext(AuthContext);
+  const { isAuthenticated, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout(); 
+    navigate('/'); 
+  };
   
   return (
     <header className="primary-header">
@@ -16,9 +22,12 @@ const Header = () => {
         </ul>
       </nav>
       {isAuthenticated ? (
-        <NavLink to="/profile" aria-label="Go to Profile">
-          <button>Profile</button>
-        </NavLink>
+        <>
+          <NavLink to="/profile" aria-label="Go to Profile">
+            <button>Profile</button>
+          </NavLink>
+          <button onClick={handleLogout} aria-label="Logout">Logout</button>
+        </>
       ) : (
         <NavLink to="/signup" aria-label="Create a new account">
           <button>Create Account</button>
